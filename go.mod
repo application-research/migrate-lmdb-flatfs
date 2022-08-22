6f27fab5add237c6a6b2019bf41323fab9d83f23
Elijah Seed-Arita <elijaharita@gmail.com>
2022-08-22 13:20:57 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,60 @@
+module github.com/application-research/migrate-lmdb-flatfs
+
+go 1.19
+
+require (
+	github.com/cheggaaa/pb/v3 v3.0.8
+	github.com/filecoin-project/go-bs-lmdb v1.0.6-0.20211215050109-9e2b984c988e
+	github.com/ipfs/go-ds-flatfs v0.5.1
+	github.com/ipfs/go-ipfs-blockstore v1.1.2
+	github.com/urfave/cli/v2 v2.3.0
+)
+
+require (
+	github.com/VividCortex/ewma v1.1.1 // indirect
+	github.com/alexbrainman/goissue34681 v0.0.0-20191006012335-3fc7a47baff5 // indirect
+	github.com/benbjohnson/clock v1.2.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/ipfs/bbloom v0.0.4 // indirect
+	github.com/ipfs/go-block-format v0.0.3 // indirect
+	github.com/ipfs/go-cid v0.1.0 // indirect
+	github.com/ipfs/go-datastore v0.5.1 // indirect
+	github.com/ipfs/go-ipfs-ds-help v1.1.0 // indirect
+	github.com/ipfs/go-ipfs-util v0.0.2 // indirect
+	github.com/ipfs/go-log v1.0.5 // indirect
+	github.com/ipfs/go-log/v2 v2.5.0 // indirect
+	github.com/ipfs/go-metrics-interface v0.0.1 // indirect
+	github.com/jbenet/goprocess v0.1.4 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/ledgerwatch/lmdb-go v1.18.2 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/multiformats/go-base32 v0.0.4 // indirect
+	github.com/multiformats/go-base36 v0.1.0 // indirect
+	github.com/multiformats/go-multibase v0.0.3 // indirect
+	github.com/multiformats/go-multihash v0.1.0 // indirect
+	github.com/multiformats/go-varint v0.0.6 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
+	golang.org/x/crypto v0.0.0-20211209193657-4570a0811e8b // indirect
+	golang.org/x/sys v0.0.0-20211209171907-798191bca915 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	lukechampine.com/blake3 v1.1.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/cheggaaa/pb/v3"
+	lmdb "github.com/filecoin-project/go-bs-lmdb"
+	blocks "github.com/ipfs/go-block-format"
+	flatfs "github.com/ipfs/go-ds-flatfs"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Usage = "[lmdb blockstore path]"
+	app.Description = "Replaces an lmdb blockstore with a new unprefixed flatfs blockstore and creates a backup of the old lmdb blockstore"
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:  "sharding-function",
+			Value: "/repo/flatfs/shard/v1/next-to-last/3",
+		},
+	}
+	app.Action = func(ctx *cli.Context) error {
+		maybeRelativePath := ctx.Args().Get(0)
+		absolutePath, err := filepath.Abs(maybeRelativePath)
+		if err != nil {
+			return fmt.Errorf("invalid blockstore path %s: %v", maybeRelativePath, err)
+		}
+
+		fmt.Printf("Attempting to migrate %s\n", absolutePath)
+
+		// Attempt to open the lmdb blockstore to be migrated
+		lmdbBlockstoreSize, err := dirSize(absolutePath)
+		if err != nil {
+			return fmt.Errorf("could not stat blockstore: %v", err)
+		}
+		lmdbBlockstore, err := openOldLMDBBlockstore(absolutePath)
+		if err != nil {
+			return err
+		}
+
+		// Create the flatfs blockstore to write into in the tmp directory
+		tmpFlatfsBlockstorePath := path.Join(
+			path.Dir(absolutePath),
+			fmt.Sprintf("migrate-lmdb-flatfs-tmp-%d", rand.Uint32()),
+		)
+		flatfsBlockstore, flatfsBlockstoreCloser, err := createTmpFlatfsBlockstore(
+			tmpFlatfsBlockstorePath,
+			ctx.String("sharding-function"),
+		)
+		if err != nil {
+			return err
+		}
+
+		// Move all the blocks from lmdb blockstore to flatfs blockstore
+		fmt.Printf("Writing blocks...\n")
+		if err := transferBlocks(ctx.Context, lmdbBlockstore, flatfsBlockstore, lmdbBlockstoreSize); err != nil {
+			return err
+		}
+		fmt.Printf("Done\n")
+
+		if err := flatfsBlockstoreCloser(); err != nil {
+			return fmt.Errorf("failed to close flatfs blockstore: %v", err)
+		}
+
+		// Move the old blockstore to the backup location so the new blockstore can be placed
+		backupPath := strings.TrimRight(absolutePath, "/\\") + "-backup"
+		fmt.Printf("Moving old blockstore to backup path '%s'\n", backupPath)
+		if err := os.Rename(absolutePath, backupPath); err != nil {
+			return fmt.Errorf("failed to move old blockstore to backup location: %v", err)
+		}
+
+		// Move the new blockstore to the target location
+		fmt.Printf("Moving new blockstore to target path '%s'\n", absolutePath)
+		if err := os.Rename(tmpFlatfsBlockstorePath, absolutePath); err != nil {
+			return fmt.Errorf("failed to move new blockstore to target location: %v", err)
+		}
+
+		fmt.Printf("Finished\n")
+
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Printf("%v\n", err)
+	}
+}
+
+func dirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	})
+	return size, err
+}
+
+func transferBlocks(
+	ctx context.Context,
+	from blockstore.Blockstore,
+	to blockstore.Blockstore,
+	size int64,
+) error {
+
+	allLMDBKeys, err := from.AllKeysChan(ctx)
+	if err != nil {
+		return fmt.Errorf("could not get all lmdb keys channel: %v", err)
+	}
+
+	var buffer []blocks.Block
+
+	bar := pb.New64(size).Set(pb.Bytes, true).Set(pb.CleanOnFinish, true)
+	bar.Start()
+	defer bar.Finish()
+
+	for cid := range allLMDBKeys {
+		block, err := from.Get(ctx, cid)
+		if err != nil {
+			return fmt.Errorf("could not get expected block '%s' from lmdb blockstore: %v", cid, err)
+		}
+
+		buffer = append(buffer, block)
+		if len(buffer) >= 100 {
+			if err := to.PutMany(ctx, buffer); err != nil {
+				return fmt.Errorf("could not write block '%s' to flatfs blockstore: %v", cid, err)
+			}
+			buffer = nil
+		}
+
+		bar.Add(len(block.RawData()))
+	}
+
+	return nil
+}
+
+func openOldLMDBBlockstore(blockstorePath string) (blockstore.Blockstore, error) {
+	lmdbBlockstore, err := lmdb.Open(&lmdb.Options{
+		Path:   blockstorePath,
+		NoSync: true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("could not open the target directory as an lmdb blockstore: %v", err)
+	}
+
+	return lmdbBlockstore, nil
+}
+
+func createTmpFlatfsBlockstore(blockstorePath string, shardingFunctionString string) (blockstore.Blockstore, func() error, error) {
+	shardingFunction, err := flatfs.ParseShardFunc(shardingFunctionString)
+	if err != nil {
+		return nil, nil, fmt.Errorf("invalid sharding function '%s': %v", shardingFunctionString, err)
+	}
+	fmt.Printf("Creating new temporary flatfs blockstore at '%s'\n", blockstorePath)
+	flatfsDatastore, err := flatfs.CreateOrOpen(blockstorePath, shardingFunction, true)
+	if err != nil {
+		return nil, nil, fmt.Errorf("could not create temporary flatfs blockstore: %v", err)
+	}
+
+	return blockstore.NewBlockstoreNoPrefix(flatfsDatastore), flatfsDatastore.Close, nil
+}
